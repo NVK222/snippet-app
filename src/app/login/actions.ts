3abd5521be0c04c3068bc6bfb2ae456fd09b00d5
NVK222 <neerajvk2004@gmail.com>
2026-01-14 17:13:32 +0530

    Fixed signup typo

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -9,7 +9,7 @@ export async function signup(formData: FormData) {
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
 
-  const { error } = await supabase.auth.signup({ email, password });
+  const { error } = await supabase.auth.signUp({ email, password });
 
   if (error) {
     return redirect("/login?error=true");
