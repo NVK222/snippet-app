1d2a68acd335ad4abcd236f20fd3b86692f819f0
NVK222 <neerajvk2004@gmail.com>
2026-01-15 16:56:50 +0530

    Added updateSnippet action

--- src/app/dashboard/actions.ts
+++ src/app/dashboard/actions.ts
@@ -2,6 +2,7 @@
 
 import { createClient } from "@/utils/supabase/server";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export async function deleteSnippet(formData: FormData) {
   const supabase = await createClient();
@@ -12,4 +13,33 @@ export async function deleteSnippet(formData: FormData) {
   }
 
   revalidatePath("/dashboard");
+}
+
+export async function updateSnippet(formData: FormData) {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    return redirect("/login");
+  }
+
+  const id = formData.get("id") as string;
+  const title = formData.get("title") as string;
+  const code = formData.get("code") as string;
+  const language = formData.get("language") as string;
+  const is_public = formData.get("is_public") === "on";
+  const { error } = await supabase
+    .from("snippets")
+    .update({ code, title, language, is_public })
+    .eq("id", id)
+    .eq("user_id", user.id);
+
+  if (error) {
+    return redirect("/dashboard?error=true");
+  }
+
+  revalidatePath("/dashboard");
+  redirect("/dashboard");
 }
